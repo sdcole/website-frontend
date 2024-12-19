22c1ef1a25c3a9410fea5b681cb6e0b8048109d3
Saebastion Cole <99359888+sdcole@users.noreply.github.com>
2024-12-18 22:10:49 -0600

    Updated to use tracker api

--- personalsite-frontend/src/App.tsx
+++ personalsite-frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import './App.css'
 import { Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
@@ -10,13 +12,29 @@ import Footer from './components/Footer';
 function App() {
   const location = useLocation();
   
+  const [visitorCount, setVisitorCount] = useState<number | null>(null);
+
+  const makePostCall = async (): Promise<void> => {
+    try {
+      const response = await axios.post<number>('http://nullbyte.access.ly/api/v1/visitor-tracker');
+      const result = response.data;
+      setVisitorCount(result); // Store the result in the state
+    } catch (error) {
+      console.error('Error fetching visitor count:', error);
+    }
+  };
+
+  useEffect(() => {
+    // Call the API only once when the app is first loaded
+    makePostCall();
+  }, []);  // Empty dependency array to call it only once on initial load
   return (
     <div className='container'>
       <Navbar />
       <div className='content-container'>
       <div className="fade-container" key={location.pathname}>
           <Routes location={location}>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home visitorCount={visitorCount}/>} />
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects/>}/>
             <Route path="/contact" element={<Contact/>}/>
--- personalsite-frontend/src/pages/Home.tsx
+++ personalsite-frontend/src/pages/Home.tsx
@@ -2,30 +2,37 @@ import { Typography } from '@mui/material';
 import TopPageCode from '../components/TopPageCode';
 import BottomPageCode from '../components/BottomPageCode';
 
-const Home = () => {
+interface HomeProps {
+    visitorCount: number | null;
+  }
 
-    return (
-        <div className='page-container'>
-            <TopPageCode/>
+const Home = ({visitorCount}: HomeProps) => {
 
-            <div className='page-content'>
-                <Typography variant="h2" component="h1" className="header" gutterBottom>
-                    Saebastion Cole
-                </Typography>
-                <Typography variant="body1" paragraph>
-                    Hi, I'm Saebastion, welcome to my personal site.
-                </Typography>
-                <Typography variant="body1" paragraph>
-                    I am a software engineer currently specializing in batch file-driven and full-stack applications.
-                </Typography>
-                <Typography variant="body1" paragraph>
-                    Take a look around!
-                </Typography>
-            </div>
+  return (
+    <div className="page-container">
+      <TopPageCode />
 
-            <BottomPageCode/>
-        </div>
-    );
+      <div className="page-content">
+        <Typography variant="h2" component="h1" className="header" gutterBottom>
+          Saebastion Cole
+        </Typography>
+        <Typography variant="body1" paragraph>
+          Hi, I'm Saebastion, welcome to my personal site.
+        </Typography>
+        <Typography variant="body1" paragraph>
+          This site has had {visitorCount !== null ? visitorCount : '...'} visitors
+        </Typography>
+        <Typography variant="body1" paragraph>
+          I am a software engineer currently specializing in batch file-driven and full-stack applications.
+        </Typography>
+        <Typography variant="body1" paragraph>
+          Take a look around!
+        </Typography>
+      </div>
+
+      <BottomPageCode />
+    </div>
+  );
 };
 
 export default Home;
