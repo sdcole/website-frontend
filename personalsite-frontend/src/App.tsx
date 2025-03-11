e7cf8b627842c6d78313dc46d5e2dddb053a346a
Saebastion Cole <99359888+sdcole@users.noreply.github.com>
2025-03-10 19:14:51 -0500

    updated to my new domain

--- personalsite-frontend/src/App.tsx
+++ personalsite-frontend/src/App.tsx
@@ -10,6 +10,22 @@ import Footer from './components/Footer';
 function App() {
   const location = useLocation();
   
+  const [visitorCount, setVisitorCount] = useState<number | null>(null);
+
+  const makePostCall = async (): Promise<void> => {
+    try {
+      const response = await axios.post<number>('https://api.meshservice.work/api/v1/visitor-tracker');
+      const result = response.data;
+      setVisitorCount(result); // Store the result in the state
+    } catch (error) {
+      console.error('Error fetching visitor count:', error);
+    }
+  };
+
+  useEffect(() => {
+    // Call the API only once when the app is first loaded
+    makePostCall();
+  }, []);  // Empty dependency array to call it only once on initial load
   return (
     <div className='container'>
       <Navbar />
